13524a3954ec56ce1fe42db9c70b3c937dc28c63
isaacjs <isaacseijas7@gmail.com>
2019-04-01 13:07:42 -0400

    epa

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,26 +2,39 @@
 
 namespace App\Providers;
 
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Validator;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
-
     /**
      * Bootstrap any application services.
      *
      * @return void
      */
     public function boot()
+    {
+        Schema::defaultStringLength(191);
+
+        Validator::extend('current_password', function ($attribute, $value, $parameters, $validator) {
+            return Hash::check($value, auth()->user()->password);
+        });
+
+        Carbon::setLocale(config('app.locale'));
+
+        setlocale(LC_TIME, 'es_ES.utf8');
+
+    }
+
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
     {
         //
     }
